b2bd6ac86678d2c8da7fa57308b877cdde40bf1f
Wragnardev <seker@halithamza.software>
2022-08-30 21:28:45 +0300

    push

--- video.py
+++ video.py
@@ -1,12 +1,18 @@
 
-
 import numpy as np
 import cv2
-cap = cv2.VideoCapture('videos/wa.avi')
-while(cap.isOpened()):
-  ret, frame = cap.read()
-  gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-  cv2.imshow('frame',gray)
+
+frameCounter = 0
+
+cap = cv2.VideoCapture('movie_002.mp4')
+
+while True:
+  _, frame = cap.read()
+  if frame is None:
+    break
+  cv2.imshow('app', frame)
+  frameCounter += 1
+  print(frameCounter)
   if cv2.waitKey(1) & 0xFF == ord('q'):
     break
 
